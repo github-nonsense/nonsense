7c821d5e13ebe0d566a16bf919e21370c3cfb409
tt@tt.com <tt@tt.com>
2018-10-10 23:38:42 +0800

    working poj/1030.cc

--- pojs/1030.cc
+++ pojs/1030.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstdio>
+#include <cmath>
 
 using namespace std;
 
@@ -8,12 +8,29 @@ int a()
 	return 0;
 }
 
+void parse_line(string& s)
+{
+	int id = 0;
+	int shift = 0;
+	for (int i = 0; i < s.length(); i++) {
+		if (!isdigit(s[i])) {
+			id = 0;
+			shift = 0;
+			continue;
+		}
+
+		id += pow(10, shift) * (s[i] - '0');
+	}
+}
 
 int main()
 {
-	while (getline(str, cin)) {
-		if (!str.size)
-			continue;
+	int n;
+	string str;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		getline(cin, str);
 	}
 	return 0;
 }
