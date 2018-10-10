9c498933ff3332db0e110ef8dcf6d644aa470caa
tt@tt.com <tt@tt.com>
2018-10-10 23:25:22 +0800

    working poj/1030

---
+++ pojs/1030.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int a()
+{
+	return 0;
+}
+
+
+int main()
+{
+	while (getline(str, cin)) {
+		if (!str.size)
+			continue;
+	}
+	return 0;
+}
